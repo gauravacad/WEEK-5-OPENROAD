d5c31db69db92876a0f644a9524ca1427cda705a
Gaurav VLSI-VSD <gp.bits@gmail.com>
2025-10-25 11:10:18 +0000

    Add files via upload

---
+++ Files_needed/gcd_nangate45_Copy.tcl
@@ -0,0 +1,16 @@
+# gcd flow pipe cleaner
+source "helpers.tcl"
+source "flow_helpers.tcl"
+source "Nangate45/Nangate45.vars"
+
+set design "gcd"
+set top_module "gcd"
+set synth_verilog "gcd_nangate45.v"
+set sdc_file "gcd_nangate45.sdc"
+set die_area {0 0 100.13 100.8}
+set core_area {10.07 11.2 90.25 91}
+
+#include -echo "flow_floorplan.tcl"
+#include -echo "flow_pdn.tcl"
+#include -echo "flow_global_placement.tcl"
+include -echo "flow_detailed_placement.tcl"
